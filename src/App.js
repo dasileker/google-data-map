7f43c6839e070ed4b98431892040353e4efbc0b2
dasileker <dasileker@gmail.com>
2022-04-26 15:25:37 +0000

    added the data for google-map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import GoogleMapReact from 'google-map-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+// class SimpleMap extends Component {
+//   static defaultProps = {
+//     center: {
+//       lat: 59.95,
+//       lng: 30.33
+//     },
+//     zoom: 11
+//   };
+
+//   render() {
+//     return (
+//       // Important! Always set the container height explicitly
+//       <div id="container">
+//         <GoogleMapReact
+//           bootstrapURLKeys={{ key: 'AIzaSyBafIk-hFGrMHHaHkR6_evN0Um8-XRdT-I' }}
+//           defaultCenter={this.props.center}
+//           defaultZoom={this.props.zoom}
+//         >
+//           <AnyReactComponent
+//             lat={59.955413}
+//             lng={30.337844}
+//             text="My Marker"
+//           />
+//         </GoogleMapReact>
+//       </div>
+//     );
+//   }
+// }
+
+// export default SimpleMap;
+
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+  width: '90%',
+  height: '90%'
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: 31.794525,
+            lng: -7.0849336
+          }
+        }
+      />
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBafIk-hFGrMHHaHkR6_evN0Um8-XRdT-I'
+})(MapContainer);
